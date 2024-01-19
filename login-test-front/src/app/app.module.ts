4358f259aeaedae663e43cf930e5bcc0f054d8be
Theo Pierson <thheopierson@gmail.com>
2024-01-19 11:58:24 +0100

    feat: start angular project

--- login-test-front/src/app/app.module.ts
+++ login-test-front/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeContentComponent } from './welcome-content/welcome-content.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { ContentComponent } from './content/content.component';
+import { AuthContentComponent } from './auth-content/auth-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeContentComponent,
+    LoginFormComponent,
+    ContentComponent,
+    AuthContentComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
